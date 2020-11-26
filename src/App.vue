b4de78be7290a9ba44d32af313fb18de8933d509
Unknown <38399178+I0xAF@users.noreply.github.com>
2020-11-26 22:58:30 +0600

    not visible changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <b-container id="app">
-        <b-navbar type="light" variant="light" class="mb-1">
+        <b-navbar type="light" variant="light" class="mb-2">
             <b-navbar-brand to="/">Vimefinder</b-navbar-brand>
             <b-input-group>
               <b-form-input placeholder="Ник" @keyup.enter="redirectByEnter" v-model="search"/>
               <b-input-group-append>
-                <b-button @click="search = ''" :disabled="!maySearch" :to="'/user/' + search" variant="outline-primary">
+                <b-button @click="search = ''" :disabled="!maySearch" :to="`/user/${this.search}`" variant="outline-primary">
                   <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
                     <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
@@ -52,7 +52,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
